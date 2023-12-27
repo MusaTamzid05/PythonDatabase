acef953c635d3047e3a89773c0564ea5bc15a86f
MusaTamzid05 <tamzid101@gmail.com>
2023-12-28 00:37:15 +0600

    remove the custom id

--- main.py
+++ main.py
@@ -6,45 +6,44 @@ if __name__ == "__main__":
     database = Database(path="test.db")
     database.connect()
 
-    table_structure={"name":"text","id":"int"}
-    db = Table(table_name="test", database=database)
-    #db.create(table_structure=table_structure)
-    print(db.get_scheme())
+    table_structure={"name":"text","city":"text"}
+    table = Table(table_name="test", database=database)
+    table.create(table_structure=table_structure)
 
 
-    h1 = {"name" : "Superman", "id" : 1}
-    h2 = {"name" : "Spiderman", "id" : 2}
-    h3 = {"name": "Batman","id":3}
+    h1 = {"name" : "Superman", "city" : "metropolis"}
+    h2 = {"name" : "Spiderman", "city" : "New york"}
+    h3 = {"name": "Batman","city": "Gotham"}
 
-    h4={"name" : "Iron Man" , "id": 4}
+    h4={"name" : "Iron Man" , "city": "New york"}
 
-    db.insert(h1)
-    db.insert(h2)
-    db.insert(h3)
+    table.insert(h1)
+    table.insert(h2)
+    table.insert(h3)
 
-    rows = db.get_all_rows()
+    rows = table.get_all_rows()
     print("1.After inserting ..")
-    db.show_rows_of(rows)
+    table.show_rows_of(rows)
 
     print("data with id 1")
-    print(db.select_by_id(1))
+    print(table.select_by_id(1))
 
 
 
     print("2.After deleting {}".format(h2['name']))
-    db.delete_row(h2)
-    rows = db.get_all_rows()
-    db.show_rows_of(rows)
+    table.delete_row(h2)
+    rows = table.get_all_rows()
+    table.show_rows_of(rows)
 
 
 
     print("3.After updating {} to {}".format(h1['name'],h2['name']))
-    db.update(h1,h4)
-    rows = db.get_all_rows()
-    db.show_rows_of(rows)
+    table.update(h1,h4)
+    rows = table.get_all_rows()
+    table.show_rows_of(rows)
 
     print("4.After getting Batman")
-    rows = db.select_where(h3)
+    rows = table.select_where(h3)
 
-    db.show_rows_of(rows)
+    table.show_rows_of(rows)
 
