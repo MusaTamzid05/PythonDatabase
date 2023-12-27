7c5e62714c9361e41d2aafa965587fa6f0b62cf2
MusaTamzid05 <tamzid101@gmail.com>
2023-12-27 20:57:09 +0600

    Fix table space, and create function added

--- simple_database/database.py
+++ simple_database/database.py
@@ -10,24 +10,24 @@ import sqlite3
 
 def raise_error_if_not_dict(dict_row):
 
-	if type(dict_row) != dict:
-		raise TypeError("Data type must be a dict")
+    if type(dict_row) != dict:
+        raise TypeError("Data type must be a dict")
 
 def get_keys_values(dict_row):
-	'''
-	function to return key and values from dict
+    '''
+    function to return key and values from dict
 
 
-	'''
+    '''
 
-	raise_error_if_not_dict(dict_row)
-	keys,values=[],[]
+    raise_error_if_not_dict(dict_row)
+    keys,values=[],[]
 
-	for key,value in dict_row.items():
-		keys.append(key)
-		values.append(value)
+    for key,value in dict_row.items():
+        keys.append(key)
+        values.append(value)
 
-	return keys,values
+    return keys,values
 
 
 
@@ -36,210 +36,204 @@ def get_keys_values(dict_row):
 
 
 class Table:
+    def __init__(self, table_name,  database_name='test.db'):
+        self.database_name = database_name
+        self.table_name = table_name
 
-	def __init__(self,table_name,table_structure,database_name='test.db'):
+    def create(self, table_structure):
+        self.data_names,self.datatypes=get_keys_values(table_structure)
+        self.db = sqlite3.connect(self.database_name)
+        self.db.row_factory = sqlite3.Row # this makes sures the cursor returns row objects instance tuple
+        self.cursor = self.db.cursor()
 
-		self.table_name = table_name
-		self.data_names,self.datatypes=get_keys_values(table_structure)
+        self.cursor.execute("SELECT name FROM sqlite_master WHERE type ='table' AND name = ?",(self.table_name,))
+        table_exists = self.cursor.fetchone()
 
 
-		self.db =sqlite3.connect(database_name)
-		self.db.row_factory = sqlite3.Row # this makes sures the cursor returns row objects instance tuple
-		self.cursor = self.db.cursor()
+        if table_exists == None:
+            query="CREATE TABLE {} ".format(self.table_name)
+            temp=''
 
+            for dataname,datatype in zip(self.data_names,self.datatypes):
+                temp+=(dataname+" "+datatype+",")
 
 
-		self.cursor.execute("SELECT name FROM sqlite_master WHERE type ='table' AND name = ?",(self.table_name,))
-		table_exists = self.cursor.fetchone()
+            temp="(" + temp.strip(',') + ")"
+            query += temp
 
-		if table_exists == None:
 
-			'''
-			if the table already does not exists , it creates the table with the given staructure
-			'''
+            print("Creating table {}".format(self.table_name))
+            self.cursor.execute(query)
+            print("table created")
 
 
-			query="CREATE TABLE {} ".format(self.table_name)
-			temp=''
 
-			for dataname,datatype in zip(self.data_names,self.datatypes):
-				temp+=(dataname+" "+datatype+",")
 
+    def insert(self,row_dict):
 
-			temp="(" + temp.strip(',') + ")"
-			query += temp
+        keys,values=get_keys_values(row_dict)
+        query = "INSERT INTO {}".format(self.table_name) + "("
+        converter = Str_List_Converter()
+        string = converter.list_to_string(word_list = keys,concate_with=',')
+        query+= string+ ")"
 
+        string = ''
 
-			print("Creating table {}".format(self.table_name))
-			self.cursor.execute(query)
-			print("table created")
+        for index in range(len(keys)):
+            string += '?,'
 
+        string=string.strip(',')
+        query  += " VALUES (" + string +")"
+        insert_values=tuple(values)
 
-	def insert(self,row_dict):
 
-		keys,values=get_keys_values(row_dict)
-		query = "INSERT INTO {}".format(self.table_name) + "("
-		converter = Str_List_Converter()
-		string = converter.list_to_string(word_list = keys,concate_with=',')
-		query+= string+ ")"
 
-		string = ''
 
-		for index in range(len(keys)):
-			string += '?,'
+        self.cursor.execute(query,insert_values)
+        self.db.commit()
 
-		string=string.strip(',')
-		query  += " VALUES (" + string +")"
-		insert_values=tuple(values)
 
+    def drop_table(self):
+        self.db.execute("drop table if exists {}".format(self.table_name,))
 
+    def delete_row(self,row_dict):
 
+        if self.data_exists(row_dict) == False:
+            print("No data name {} exists in the database to delete".format(row_dict))
+            return
 
-		self.cursor.execute(query,insert_values)
-		self.db.commit()
 
 
-	def drop_table(self):
-		self.db.execute("drop table if exists {}".format(self.table_name,))
+        keys , values = get_keys_values(row_dict)
+        query = "DELETE FROM {} ".format(self.table_name) + " WHERE "
+        query += self._make_spesific_data_query(keys,concatinate_with='and')
+        values = tuple(values)
 
-	def delete_row(self,row_dict):
+        self.db.execute(query,values)
+        self.db.commit()
 
-		if self.data_exists(row_dict) == False:
-			print("No data name {} exists in the database to delete".format(row_dict))
-			return
 
 
 
-		keys , values = get_keys_values(row_dict)
-		query = "DELETE FROM {} ".format(self.table_name) + " WHERE "
-		query += self._make_spesific_data_query(keys,concatinate_with='and')
-		values = tuple(values)
 
-		self.db.execute(query,values)
-		self.db.commit()
 
+    def select_where(self,row_dict):
 
+        '''
+        return a list of match results.
+        '''
 
+        keys , values = get_keys_values(row_dict)
+        query = "SELECT * FROM {}".format(self.table_name)+" WHERE "
+        query += self._make_spesific_data_query(keys,concatinate_with='and')
 
+        values = tuple(values)
+        self.cursor.execute(query,values)
+        results = self.cursor.fetchall()
 
+        match_results = []
 
-	def select_where(self,row_dict):
+        if results:
+            for result in results:
+                match_results.append(dict(result))
 
-		'''
-		return a list of match results.
-		'''
 
-		keys , values = get_keys_values(row_dict)
-		query = "SELECT * FROM {}".format(self.table_name)+" WHERE "
-		query += self._make_spesific_data_query(keys,concatinate_with='and')
+        return match_results
 
-		values = tuple(values)
-		self.cursor.execute(query,values)
-		results = self.cursor.fetchall()
 
-		match_results = []
 
-		if results:
-			for result in results:
-				match_results.append(dict(result))
 
 
-		return match_results
+    def update(self,olddata_dict,newdata_dict):
+        new_keys,new_values = get_keys_values(newdata_dict)
+        query = "UPDATE {}".format(self.table_name) + " SET "
+        query += self._make_spesific_data_query(new_keys)
+        query += " WHERE "
+        old_keys,old_values = get_keys_values(olddata_dict)
 
+        query += self._make_spesific_data_query(old_keys,concatinate_with ='and')
 
+        values=new_values  + old_values
+        values = tuple(values)
 
+        self.db.execute(query,values)
+        self.db.commit()
 
 
-	def update(self,olddata_dict,newdata_dict):
-		new_keys,new_values = get_keys_values(newdata_dict)
-		query = "UPDATE {}".format(self.table_name) + " SET "
-		query += self._make_spesific_data_query(new_keys)
-		query += " WHERE "
-		old_keys,old_values = get_keys_values(olddata_dict)
 
-		query += self._make_spesific_data_query(old_keys,concatinate_with ='and')
 
-		values=new_values  + old_values
-		values = tuple(values)
 
-		self.db.execute(query,values)
-		self.db.commit()
 
+    def get_all_rows(self):
 
+        '''
 
+        Return a list of dict objects where  each dict represents a row
 
+        '''
 
+        rows = []
+        temp_rows=self.db.execute("SELECT * FROM {}".format(self.table_name))
 
-	def get_all_rows(self):
+        if temp_rows:
+            for row in temp_rows:
+                rows.append(dict(row))
 
-		'''
 
-		Return a list of dict objects where  each dict represents a row
+        return rows
 
-		'''
 
-		rows = []
-		temp_rows=self.db.execute("SELECT * FROM {}".format(self.table_name))
 
-		if temp_rows:
-			for row in temp_rows:
-				rows.append(dict(row))
 
+    def _make_spesific_data_query(self,keys,concatinate_with=','):
+        string = ""
 
-		return rows
+        for key in keys:
+            string += " {} =? {}".format(key,concatinate_with)
 
+        return string.strip(concatinate_with)
 
 
 
-	def _make_spesific_data_query(self,keys,concatinate_with=','):
-		string = ""
 
-		for key in keys:
-			string += " {} =? {}".format(key,concatinate_with)
 
-		return string.strip(concatinate_with)
+    def show_rows_of(self,list_dict):
 
+        if len(list_dict):
+            for data in list_dict:
+                print(data)
 
+        else:
+            print("No rows where returned!!")
 
 
+    def data_exists(self,row_dict):
+        return True if len(self.select_where(row_dict)) else False
 
-	def show_rows_of(self,list_dict):
 
-		if len(list_dict):
-			for data in list_dict:
-				print(data)
 
-		else:
-			print("No rows where returned!!")
+    def select_by_id(self,id_):
 
+        if type(id_) != int:
+            raise TypeError("Id must be a int")
 
-	def data_exists(self,row_dict):
-		return True if len(self.select_where(row_dict)) else False
+        row_dict={"ROWID" :id_}
 
+        row = self.select_where(row_dict)
 
+        if len(row) == 0:
+            print("No data with id {}".format(id_))
 
-	def select_by_id(self,id_):
+        elif len(row) > 1:
+            print("Warrning: {} data with id {} was found.".format(len(row),id_))
 
-		if type(id_) != int:
-			raise TypeError("Id must be a int")
+        return row[0]
 
-		row_dict={"ROWID" :id_}
 
-		row = self.select_where(row_dict)
-
-		if len(row) == 0:
-			print("No data with id {}".format(id_))
-
-		elif len(row) > 1:
-			print("Warrning: {} data with id {} was found.".format(len(row),id_))
-
-		return row[0]
-
-
-	def last_insert_id(self):
-		query = "SELECT MAX(ROWID) FROM {}".format(self.table_name)
-		result=dict(self.cursor.execute(query).fetchone())
-		return result['MAX(ROWID)']
+    def last_insert_id(self):
+        query = "SELECT MAX(ROWID) FROM {}".format(self.table_name)
+        result=dict(self.cursor.execute(query).fetchone())
+        return result['MAX(ROWID)']
 
 
 
@@ -247,46 +241,46 @@ class Table:
 
 if __name__ == "__main__":
 
-	table_structure={"name":"text","id":"int"}
+    table_structure={"name":"text","id":"int"}
 
-	db = Table("test",table_structure=table_structure)
-
-
-	h1 = {"name" : "Superman", "id" : 1}
-	h2 = {"name" : "Spiderman", "id" : 2}
-	h3 = {"name": "Batman","id":3}
-
-	h4 = {"name" : "Iron Man" , "id": 4}
+    db = Table("test",table_structure=table_structure)
 
 
-	db.insert(h1)
-	db.insert(h2)
-	db.insert(h3)
+    h1 = {"name" : "Superman", "id" : 1}
+    h2 = {"name" : "Spiderman", "id" : 2}
+    h3 = {"name": "Batman","id":3}
+
+    h4 = {"name" : "Iron Man" , "id": 4}
 
 
-	rows = db.get_all_rows()
-	print("1.After inserting ..")
-	db.show_rows_of(rows)
-
-	print("data with id 1")
-	print(db.select_by_id(1))
+    db.insert(h1)
+    db.insert(h2)
+    db.insert(h3)
 
 
+    rows = db.get_all_rows()
+    print("1.After inserting ..")
+    db.show_rows_of(rows)
 
-	print("2.After deleting {}".format(h2['name']))
-	db.delete_row(h2)
-	rows = db.get_all_rows()
-	db.show_rows_of(rows)
+    print("data with id 1")
+    print(db.select_by_id(1))
 
 
 
-	print("3.After updating {} to {}".format(h1['name'],h2['name']))
-	db.update(h1,h4)
-	rows = db.get_all_rows()
-	db.show_rows_of(rows)
+    print("2.After deleting {}".format(h2['name']))
+    db.delete_row(h2)
+    rows = db.get_all_rows()
+    db.show_rows_of(rows)
 
-	print("4.After getting Batman")
-	rows = db.select_where(h3)
 
-	db.show_rows_of(rows)
+
+    print("3.After updating {} to {}".format(h1['name'],h2['name']))
+    db.update(h1,h4)
+    rows = db.get_all_rows()
+    db.show_rows_of(rows)
+
+    print("4.After getting Batman")
+    rows = db.select_where(h3)
+
+    db.show_rows_of(rows)
 
