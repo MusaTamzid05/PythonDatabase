ba88f9c87bba2e437d5a49edd86c4ee2f99700f3
MusaTamzid05 <tamzid101@gmail.com>
2024-01-05 19:47:40 +0600

    showing table data

--- server.py
+++ server.py
@@ -9,7 +9,6 @@ import os
 from simple_database.database import Database
 from simple_database.database import Table
 
-current_database = None
 
 
 
@@ -17,6 +16,21 @@ DATABASE_DIR_PATH = os.path.join(os.getcwd(), "databases")
 
 app = Flask(__name__)
 app.secret_key = "this is the world best secret key"
+
+current_database_name = None
+
+def get_current_database():
+    global current_database_name
+    global DATABASE_DIR_PATH
+
+    database_path = os.path.join(DATABASE_DIR_PATH, current_database_name)
+
+    current_database = Database(path=database_path)
+    current_database.connect()
+
+    return current_database
+
+
 
 def get_database_list():
     return os.listdir(DATABASE_DIR_PATH)
@@ -51,12 +65,11 @@ def home():
 
 @app.route("/database/<database_name>", methods=["GET"])
 def show_database(database_name):
-    global current_database
-    database_path = os.path.join(DATABASE_DIR_PATH, database_name)
-
+    global current_database_name
+    current_database_name = database_name
+    current_database = get_current_database()
     database_list = get_database_list()
-    current_database = Database(path=database_path)
-    current_database.connect()
+
     table_names = current_database.get_table_names()
 
     context = {
@@ -70,12 +83,25 @@ def show_database(database_name):
 
 
 
-@app.route("/table/<table_name>/<offset>", methods=["GET"])
-def show_table(table_name, offset):
+@app.route("/table/<table_name>/<page>", methods=["GET"])
+def show_table(table_name, page):
     database_list = get_database_list()
-    print(f"Offset {offset}")
+    print(f"page {page} {table_name}")
     context = {"database_list" : database_list}
     context["table_name"] = table_name
+
+    current_database = get_current_database()
+
+    table = Table(table_name=table_name, database=current_database)
+    rows = table.get_data_of_index(start_index=0, end_index=10)
+    context["col_names"] = list(rows[0].keys())
+
+
+    for col_name in context["col_names"]:
+        print(col_name)
+
+    context["rows"] = rows
+    print(rows)
 
     return render_template("show_table.html", context=context)
 
